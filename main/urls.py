19f7f5ca02f5e4ef05d194fe8012275356e9edfa
Nikita Turda <nikitosturda5@gmail.com>
2023-07-18 20:09:18 +0300

    Sign up is now working properly

--- main/urls.py
+++ main/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.frontpage, name='main')
+    path('', views.frontpage, name='main'),
+    path('signup/', views.signup, name='signup')
 ]
--- main/views.py
+++ main/views.py
@@ -1,5 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import SignUpForm
+from django.contrib.auth import login
 
 
 def frontpage(request):
     return render(request, 'main/main.html')
+
+
+def signup(request):
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+
+        if form.is_valid():
+            user = form.save()
+
+            login(request, user)
+
+            return redirect('main')
+    else:
+        form = SignUpForm()
+
+    return render(request, 'main/signup.html', {'form': form})
